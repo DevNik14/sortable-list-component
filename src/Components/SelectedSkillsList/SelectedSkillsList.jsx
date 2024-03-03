a7171e5970ba2564c25e57da0f49f894e16c93cf
DevNik14 <nykyurchiyski@gmail.com>
2024-03-03 14:00:51 +0200

    feat: allow user to select skill from a select option

--- src/Components/SelectedSkillsList/SelectedSkillsList.jsx
+++ src/Components/SelectedSkillsList/SelectedSkillsList.jsx
@@ -9,49 +9,59 @@ import 'bootstrap-icons/font/bootstrap-icons.css';
 
 const SelectedSkillsList = () => {
   const suggestedSKillsList = ['HTML', 'CSS', 'Bootstrap', 'TypeScript', 'Vue', 'Angular'];
-  const [selectedSKillList, setSelectedSKillList] = useState([]);
+  const [selectedSKillList, setSelectedSKillList] = useState({
+    firstSkill: '',
+    secondSkill: '',
+    thirdSkill: '',
+    fourthSkill: '',
+    fifthSkill: ''
+  });
 
   const selectSkillHandler = (e) => {
-    const selectedSKill = e.currentTarget.textContent;
-    if(selectedSKillList.includes(selectedSKill)) {
+    const selectedSKill = e.target.value;
+    if (Object.values(selectedSKillList).includes(selectedSKill)) {
       console.log('This skill already have been added');
     } else {
-      setSelectedSKillList(oldState => [...oldState, selectedSKill]);
+      setSelectedSKillList(oldState => (
+        {...oldState, [e.target.name]: selectedSKill}
+      ));
     }
   }
 
   const deleteSkillHandler = (e) => {
-    const skillToBeDeleted = e.currentTarget.parentNode.textContent.split('. ')[1];
-    setSelectedSKillList(oldState => oldState.filter(skill => skill !== skillToBeDeleted));
+    const skillToBeDeleted = e.currentTarget.parentNode.classList[1];
+    setSelectedSKillList(oldState => (
+      {...oldState, [skillToBeDeleted]: ''}
+    ));
   }
 
   return (
     <section className={`${styles.selectedSkillsListWrapper}`}>
       <ul className={`${styles.skills}`}>
-        {selectedSKillList.map((skill, i) => <li key={skill}>
-          <div className={`${styles.selectedSkill}`}>
-          {i + 1}. {skill}
-          <Delete deleteHandler={deleteSkillHandler}/>
-        </div>
-        </li>)}
-        <div className={`${styles.customSelect}`}>
-        <Down />
-          <select name="">
-            <option>ReactJS</option>
-            <option>React Native</option>
-            <option>React Redux</option>
-          </select>
-        </div>
-        <div className={`${styles.customSelect}`}>
-        <Down />
-          <select name="" disabled>
-            <option>NodeJS</option>
-            <option>ExpressJS</option>
-            <option>React Redux</option>
-          </select>
-        </div>
+        {Object.keys(selectedSKillList).map((skill, i) =>
+          <li key={skill}>
+            {selectedSKillList[skill]
+              ? <div className={`${styles.selectedSkill} ${skill}`}>
+                {i + 1}. {selectedSKillList[skill]}
+                <span onClick={deleteSkillHandler}>
+                  <Delete />
+                </span>
+              </div>
+              : <div className={`${styles.customSelect}`}>
+                <Down deleteSkillHandler={deleteSkillHandler}/>
+                <select name={skill} onChange={selectSkillHandler} value={selectedSKillList[skill]}>
+                  <option value="add skill">Add Skill</option>
+                  <option value="ReactJS">ReactJS</option>
+                  <option value="React Native">React Native</option>
+                  <option value="React Redux">React Redux</option>
+                  <option value="NodeJS">NodeJS</option>
+                </select>
+              </div>
+            }
+          </li>
+        )}
       </ul>
-      <SuggestedSkills suggestedSKillsList={suggestedSKillsList} selectSkillHandler={selectSkillHandler}/>
+      <SuggestedSkills suggestedSKillsList={suggestedSKillsList} selectSkillHandler={selectSkillHandler} />
     </section>
   )
 }
