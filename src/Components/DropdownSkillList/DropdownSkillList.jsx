344cac9df9d2f8be1310747b5f4c6abb972406e2
DevNik14 <nykyurchiyski@gmail.com>
2024-03-10 21:53:28 +0200

    refactor: conditinaly show the filtered skills list

--- src/Components/DropdownSkillList/DropdownSkillList.jsx
+++ src/Components/DropdownSkillList/DropdownSkillList.jsx
@@ -2,11 +2,14 @@ import styles from './DropdownSkillList.module.css'
 
 const DropDownSkillList = ({ filteredSkills }) => {
   return (
-    <ul className={`${styles.dropDownSkillList}`}>
-      {filteredSkills.length > 0 && filteredSkills.map(skill => (
-        <li className={`${styles.dropDownSkillListItem}`} key={skill}>{skill}</li>
-      ))}
-    </ul>
+    <>
+      {filteredSkills.length > 0 && <ul className={`${styles.dropDownSkillList}`}>
+        {filteredSkills.map(skill => (
+          <li className={`${styles.dropDownSkillListItem}`} key={skill}>{skill}</li>
+        ))}
+      </ul>
+      }
+    </>
   )
 }
 
